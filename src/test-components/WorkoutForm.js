8c3b6e7c19edc6dcb9a053dfc02aef6f8ca40c31
Leah Haake <leahhaake@Leahs-MacBook-Air.local>
2020-11-23 17:11:43 -0500

    Working on implementing a React Bootstrap Form

--- src/test-components/WorkoutForm.js
+++ src/test-components/WorkoutForm.js
@@ -1,5 +1,8 @@
 import React, {useState} from "react"
 // import {Link} from "react-router-dom"
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const WorkoutForm = (props) => {
 
@@ -7,7 +10,7 @@ const WorkoutForm = (props) => {
     const [formData, setFormData] = useState(props.workout);   // update with props sent down from app for an empty form 
     
     console.log("props", props.workout)
-    
+
     // HandleSubmit 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -21,90 +24,220 @@ const WorkoutForm = (props) => {
         setFormData({...formData, [event.target.name]: event.target.value })
     }
 
-  return (
-      <div>
-        <form onSubmit={handleSubmit}> 
-        <br />
-        <h1>WorkoutForm Component</h1>
-        Title:
-        <input
+//   return (
+//       <div>
+//         <form onSubmit={handleSubmit}> 
+//         <br />
+//         <h1>WorkoutForm Component</h1>
+//         Title:
+//         <input
+//             type="text"
+//             name="title"
+//             value={formData.title}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Date: 
+//         <input
+//             type="text"
+//             name="date"
+//             value={formData.date}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Time:
+//         <input
+//             type="number"
+//             name="time"
+//             placeholder="Enter a time"
+//             value={formData.time}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Distance: 
+//         <input
+//             type="number"
+//             name="distance"
+//             value={formData.distance}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Sport: 
+//         <input
+//             type="text"
+//             name="sport"
+//             value={formData.sport}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Description:
+//         <input
+//             type="text"
+//             name="description"
+//             value={formData.description}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Exertion:
+//         <input
+//             type="number"
+//             name="exertion"
+//             value={formData.exertion}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         URL:
+//         <input
+//             type="text"
+//             name="url"
+//             value={formData.url}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         Favorite?: 
+//         <input
+//             type="text"
+//             name="isFavorite"
+//             value={formData.isFavorite}
+//             onChange={handleChange}
+//         />
+//         <br />
+//         <input className="form-button" type="submit" value={props.label} />
+
+//         </form>
+//     </div>
+//     )
+
+return (
+    <Form onSubmit={handleSubmit}>
+    <h1>WorkoutForm Component</h1>
+
+    <Form.Row>
+    <Form.Group as={Col} controlId="">
+        <Form.Label>Title</Form.Label>
+        <Form.Control 
             type="text"
+            placeholder=""
             name="title"
             value={formData.title}
-            onChange={handleChange}
-        />
-        <br />
-        Date: 
-        <input
-            type="text"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-        />
-        <br />
-        Time:
-        <input
-            type="number"
-            name="time"
-            placeholder="Enter a time"
-            value={formData.time}
-            onChange={handleChange}
-        />
-        <br />
-        Distance: 
-        <input
-            type="number"
-            name="distance"
-            value={formData.distance}
-            onChange={handleChange}
-        />
-        <br />
-        Sport: 
-        <input
-            type="text"
-            name="sport"
-            value={formData.sport}
-            onChange={handleChange}
-        />
-        <br />
-        Description:
-        <input
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-        />
-        <br />
-        Exertion:
-        <input
-            type="number"
-            name="exertion"
-            value={formData.exertion}
-            onChange={handleChange}
-        />
-        <br />
-        URL:
-        <input
-            type="text"
-            name="url"
-            value={formData.url}
-            onChange={handleChange}
-        />
-        <br />
-        Favorite?: 
-        <input
-            type="text"
-            name="isFavorite"
-            value={formData.isFavorite}
-            onChange={handleChange}
-        />
-        <br />
-        <input className="form-button" type="submit" value={props.label} />
+            onChange={handleChange} />
+    </Form.Group>
 
-        </form>
-    </div>
-    )
+    <Form.Group as={Col} controlId="">
+      <Form.Label>Sport</Form.Label>
+      <Form.Control as="select" defaultValue="Choose..."
+        type="text"
+        name="sport"
+        value={formData.sport}
+        onChange={handleChange}
+      >
+        <option>Choose...</option>
+        <option>Walk</option>
+        <option>Hike</option>
+        <option>Run</option>
+        <option>Bike</option>
+        <option>Swim</option>
+        <option>Yoga</option>
+        <option>Pilates</option>
+        <option>Lift Weights</option>
+        <option>Crossfit</option>
+        <option>HIIT</option>
+        <option>Rock Climb</option>
+        <option>Ski</option>
+        <option>Snowboard</option>
+        <option>Snowshoe</option>
+        <option>Surf</option>
+        <option>Kayak</option>
+        <option>Stand Up Paddleboard</option>
+        <option>Workout</option>
+        </Form.Control>
+    </Form.Group>
 
+  </Form.Row>
 
+    <Form.Group as={Col} controlId="">
+    <Form.Label>Description</Form.Label>
+    <Form.Control as="textarea" rows={4} 
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="How did it go? Were you tired or rested? What was the weather?"
+    />
+  </Form.Group>
+
+<Form.Row>
+  <Form.Group as={Col} controlId="">
+    <Form.Label>Date</Form.Label>
+    <Form.Control 
+        type="text"
+        name="date"
+        value={formData.date}
+        onChange={handleChange}
+        placeholder="" />
+  </Form.Group>
+
+  <Form.Group as={Col} controlId="">
+      <Form.Label>Time</Form.Label>
+      <Form.Control 
+        type="number"
+        name="time"
+        placeholder="Enter a time"
+        value={formData.time}
+        onChange={handleChange}
+        placeholder="" />
+    </Form.Group>
+</Form.Row>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="">
+      <Form.Label>Distance</Form.Label>
+      <Form.Control
+        type="number"
+        name="distance"
+        value={formData.distance}
+        onChange={handleChange}
+        placeholder="" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="">
+      <Form.Label>URL</Form.Label>
+      <Form.Control 
+        type="text"
+        name="url"
+        value={formData.url}
+        onChange={handleChange}
+        placeholder="" />
+    </Form.Group>
+</Form.Row>
+
+    <Form.Group as={Col} controlId="">
+      <Form.Label>Exertion</Form.Label>
+      <Form.Control 
+        type="number"
+        name="exertion"
+        value={formData.exertion}
+        onChange={handleChange}
+        placeholder="" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="">
+      <Form.Label>Favorite?</Form.Label>
+      <Form.Control 
+        type="text"
+        name="isFavorite"
+        value={formData.isFavorite}
+        onChange={handleChange}
+        placeholder="" />
+    </Form.Group>
+   
+
+  <Button variant="primary" type="submit" value={props.label}>
+    Save
+  </Button>
+
+</Form>
+
+)
 }
 export default WorkoutForm;
